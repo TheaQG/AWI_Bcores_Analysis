21f51838d1a076ba6f81a9a12b6ca28badaa9b1e
TheaQG <Thea.Quistgaard@gmail.com>
2021-01-22 13:39:24 +0100

    Work on Interpolation_Class

--- Interpolation_Class.py
+++ Interpolation_Class.py
@@ -43,12 +43,70 @@ class Interpolations():
 
         return
 
-    def interpolateData(self):
+    def interpolateData(self, pad=False, valPad=0):
+    '''
+        Computes interpolation btw. depthMin-pad and depthMax+pad, or for whole core.
+        Padding is added to avoid undesired border effects.
+        Uses cubic spline interpolation with a new sample size equal to the
+        minimal sample size in the original data set.
 
-        return x_int, y_int
+
+        Arguments:
+        ----------
+            pad:            [float] Padding to avoid werid borders. Default = 1 [m].
+
+        returns:
+        --------
+            dhat:           [array of floats] Depth data corresponding to interpolated data.
+            xhat:           [array of floats] d18O data corresponding to interpolated data.
+            Delta:          [float] New sample size.
+
+    '''
+
+        x_arr = self.x_data
+        y_arr = self.y_data
+
+        if not pad:
+            xMin = self.xMin
+            xMax = self.xMax
+        else:
+            xMin = self.xMin - valPad
+            xMax = self.xMax + valPad
+
+        x = x_arr[(x_arr >= xMin) & (x_arr <= xMax)]
+        y = y_arr[(x_arr >= xMin) & (x_arr <= xMax)]
+
+        if self.DeltaInput:
+            Delta = self.samplingSize
+        else:
+            diff = np.diff(x)
+            Delta = round(min(diff), 3)
+
+        xHat_min = Delta * np.ceil(x.values[0]/Delta)
+        xHat_max = Delta * np.floor(x.values[-1]/Delta)
+
+        n = int(1 + (xHat_max - xHat_min)/Delta)
+
+        j_arr = np.linspace(0,n,n)
+        xhat0 = xHat_min + (j_arr - 1)*Delta
+
+        if self.interpType == 'CubicSpline':
+            f = interpolate.CubicSpline(x,y)
+        elif self.interpType == 'Linear':
+            f = interpolate.interp1d(x,y, kind='linear')
+
+        yHat0 = f(xHat0)
+
+        xHat = xHat0[(xHat0 >= xHat_min) & (xHat0 <= xHat_max)]
+        yHat = yHat0[(xHat0 >= xHat_min) & (xHat0 <= xH_max)]
+
+
+        return xHat, yHat, Delta
 
     def interpolateGap(self):
-
+        gapMin = self.gapMin
+        gapMax = self.gapMax
+        gapLen = gapMax - gapMin
         return
 
     def interpolateDataAndGap(self):
